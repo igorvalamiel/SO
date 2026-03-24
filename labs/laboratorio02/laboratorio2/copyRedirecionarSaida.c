26feaa0d11793cbabc41e86aa90e40c3a62f1601
Igor Valamiel Peixoto Nunes <68130156+igorvalamiel@users.noreply.github.com>
2026-03-24 16:00:57 -0300

    update lab2

--- labs/laboratorio02/laboratorio2/copyRedirecionarSaida.c
+++ labs/laboratorio02/laboratorio2/copyRedirecionarSaida.c
@@ -5,10 +5,46 @@
 //gcc hello.c -o hello
 
 int main(void){
-	pid_t child_pid = fork();
+
+	pid_t pid;
+	int mypipe[2];
+
+	/* Create the pipe. */
+	if (pipe (mypipe))
+		{
+		fprintf (stderr, "Pipe failed.\n");
+		return EXIT_FAILURE;
+		}
+
+
+	/* Create the child process. */
+	pid_t pid = fork();
+	if (pid == (pid_t) 0)
+		{
+		/* This is the child process.
+			Close other end first. */
+		close (mypipe[1]);
+		read_from_pipe (mypipe[0]);
+		return EXIT_SUCCESS;
+		}
+	else if (pid < (pid_t) 0)
+		{
+		/* The fork failed. */
+		fprintf (stderr, "Fork failed.\n");
+		return EXIT_FAILURE;
+		}
+	else
+		{
+		/* This is the parent process.
+			Close other end first. */
+		close (mypipe[0]);
+		write_to_pipe (mypipe[1]);
+		return EXIT_SUCCESS;
+		}
+
 	int fd;
 	char *filename = "saida.out";
-	if (child_pid == 0) {
+	if (pid == 0) {
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		dup2(fd, STDOUT_FILENO);
 		execlp("/bin/ping","ping","localhost","-c","10",NULL);
